aa11e2c2e115598a699ae7fc07f50fd22277e99e
ChrisManuel86 <chris.manuel86@gmail.com>
2019-04-22 20:01:32 +0000

    created help.js and role.js

---
+++ commands/help.js
@@ -0,0 +1,18 @@
+// create prefix variable for use in this file
+const { prefix } = require('../config.json');
+
+module.exports = {
+	name: 'help',
+	description: 'List all of my commands or info about a specific command.',
+	aliases: ['commands'],
+	usage: '[command name]',
+	cooldown: 5,
+	execute(message, args) {
+		const data = [];
+        const { commands } = message.client;
+
+        if (!args.length) {
+	        // ...
+        }
+	},
+};
