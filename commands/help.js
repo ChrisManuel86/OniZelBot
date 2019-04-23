ca54f076e40c6df4a8f8bbbc49757e729c14b824
ChrisManuel86 <chris.manuel86@gmail.com>
2019-04-23 20:00:17 +0000

    Added to help.js

--- commands/help.js
+++ commands/help.js
@@ -1,4 +1,4 @@
-// create prefix variable for use in this file
+// Initialize prefix variable for use in this file
 const { prefix } = require('../config.json');
 
 module.exports = {
@@ -12,7 +12,22 @@ module.exports = {
         const { commands } = message.client;
 
         if (!args.length) {
-	        // ...
+			const name = args[0].toLowerCase();
+			const command = commands.get(name) || commands.find(c => c.aliases && c.aliases.includes(name));
+
+			if (!command) {
+				return message.reply('that\'s not a valid command!');
+			}
+
+			data.push(`**Name:** ${command.name}`);
+
+			if (command.aliases) data.push(`**Aliases:** ${command.aliases.join(', ')}`);
+			if (command.description) data.push(`**Description:** ${command.description}`);
+			if (command.usage) data.push(`**Usage:** ${prefix}${command.name} ${command.usage}`);
+
+			data.push(`**Cooldown:** ${command.cooldown || 3} second(s)`);
+
+			message.channel.send(data, { split: true });
         }
 	}
 };
