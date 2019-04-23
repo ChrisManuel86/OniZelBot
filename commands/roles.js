05cd075f0ce3f06a183d2bbd216d0e3225778195
Christopher Manuel <christopher.manuel@dealerspike.com>
2019-04-23 10:40:12 -0700

    Modified help and roles files, created embed file

--- commands/roles.js
+++ commands/roles.js
@@ -2,6 +2,6 @@ module.exports = {
     name: "roles",
     description: "Choose from the available roles on the server. See #available-roles for additional info on roles.",
     execute(message, args) {
-        
+        // ...
     }
 };
