029e4299b1acb0061368e36bd8a206bf713c62f4
ChrisManuel86 <chris.manuel86@gmail,com>
2019-05-10 18:11:02 -0700

    updated help file to use embeds

--- commands/help.js
+++ commands/help.js
@@ -1,6 +1,17 @@
 // Initialize prefix variable for use in this file
 const { prefix } = require('../config.json');
 
+// Create Discord object
+const Discord = require('discord.js');
+
+// Create embedHelp object
+let embedHelp = new Discord.RichEmbed()
+	.setColor('#0099ff');
+
+// Create embedCommand object
+let embedCommand = new Discord.RichEmbed()
+	.setColor('#0099ff');
+
 module.exports = {
 	name: 'help',
 	description: 'List all of my commands or info about a specific command.',
@@ -8,23 +19,15 @@ module.exports = {
 	usage: '[command name]',
 	cooldown: 5,
 	execute(message, args) {
-		const data = [];
         const { commands } = message.client;
 
         if (!args.length) {
-			data.push('Here\'s a list of all my commands:');
-			data.push(commands.map(command => command.name).join(', '));
-			data.push(`\nYou can send \`${prefix}help [command name]\` to get info on a specific command!`);
+			embedHelp.setTitle('Help Command')
+				.setDescription('Here\'s a list of all my commands:')
+				.addField('Available Commands' ,commands.map(command => command.name).join(', '))
+				.addField('Command Usage', `You can send \`${prefix}help [command name]\` to get info on a specific command!`);
 
-			return message.author.send(data, { split: true })
-				.then(() => {
-					if (message.channel.type === 'dm') return;
-					message.reply('I\'ve sent you a DM with all my commands!');
-				})
-				.catch(error => {
-					console.error(`Could not send help DM to ${message.author.tag}.\n`, error);
-					message.reply('it seems like I can\'t DM you! Do you have DMs disabled?');
-				});
+			return message.channel.send(embedHelp);
 		}
 		console.log(args[0]);
 		const name = args[0].toLowerCase();
@@ -32,14 +35,15 @@ module.exports = {
 		
 		if (!command) {return message.reply('that\'s not a valid command!');}
 
-		data.push(`**Name:** ${command.name}`);
+		embedCommand.setTitle('**Name:**')
+			.setDescription(`${command.name}`);
 
-		if (command.aliases) data.push(`**Aliases:** ${command.aliases.join(', ')}`);
-		if (command.description) data.push(`**Description:** ${command.description}`);
-		if (command.usage) data.push(`**Usage:** ${prefix}${command.name} ${command.usage}`);
+		if (command.aliases) embedCommand.addField('**Aliases:**', `${command.aliases.join(', ')}`);
+		if (command.description) embedCommand.addField('**Description:**', `${command.description}`);
+		if (command.usage) embedCommand.addField('**Usage:**', `${prefix}${command.name} ${command.usage}`);
 
-		data.push(`**Cooldown:** ${command.cooldown || 3} second(s)`);
+		embedCommand.addField('**Cooldown:**', `${command.cooldown || 3} second(s)`);
 		
-		message.channel.send(data, { split: true });
+		message.channel.send(embedCommand);
 	}
 };
