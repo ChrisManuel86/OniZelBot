596ff59fddbe2492d30205a026c2c0cc9f382761
ChrisManuel86 <chris.manuel86@gmail.com>
2019-05-17 16:33:57 -0700

    udpate server-info.js

--- commands/server-info.js
+++ commands/server-info.js
@@ -1,12 +1,13 @@
-// Set Discord object
+// Set Discord object and embedColor object
+const embedColor = require('../constants.json');
 const Discord = require('discord.js');
 
 // Set embed object
-let embed = new Discord.RichEmbed()
-	.setColor('#0099ff');
+let embed = new Discord.RichEmbed();
+	embed.setColor(embedColor);
 
 module.exports = {
-	name: 'server',
+	name: 'server-info',
 	description: 'Display info about this server.',
 	execute(message) {
 		embed.setTitle(`Server name: ${message.guild.name}`)
