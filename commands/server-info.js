c0f77885475d4c964734b8a0042917383e7d487d
ChrisManuel86 <chris.manuel86@gmail,com>
2019-05-21 22:25:06 -0700

    Updated server-info

--- commands/server-info.js
+++ commands/server-info.js
@@ -2,11 +2,6 @@
 const {embedColorHex} = require('../constants.json');
 const Discord = require('discord.js');
 
-// Convert hex value to decimal
-let embedColorDecimal = embedColorHex;
-embedColorDecimal = parseInt(embedColorDecimal);
-
-
 // Set embed object
 let embed = new Discord.RichEmbed();
 
@@ -15,12 +10,13 @@ module.exports = {
 	description: 'Display info about this server.',
 	execute(message) {
 		embed = {
-			color: embedColorDecimal,
+			color: parseInt(embedColorHex),
 			title: `Server name: ${message.guild.name}`,
 			field: [{
 				name: 'Total members:',
 				value: `${message.guild.memberCount}`
-			}]
+			}],
+			timestamp: new Date()
 		};
 		message.channel.send({embed: embed});
 	}
