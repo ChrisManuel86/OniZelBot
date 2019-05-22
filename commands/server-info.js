761bd9b0c3c2ed52be423f3e3f3d0e49020fd412
ChrisManuel86 <chris.manuel86@gmail,com>
2019-05-21 22:38:14 -0700

    updated contants and server-info

--- commands/server-info.js
+++ commands/server-info.js
@@ -1,5 +1,5 @@
 // Set Discord object and embedColor object
-const {embedColorHex} = require('../constants.json');
+const {embedColorHex, blankField} = require('../constants.json');
 const Discord = require('discord.js');
 
 // Set embed object
@@ -12,10 +12,17 @@ module.exports = {
 		embed = {
 			color: parseInt(embedColorHex),
 			title: `Server name: ${message.guild.name}`,
-			field: [{
-				name: 'Total members:',
-				value: `${message.guild.memberCount}`
-			}],
+			fields: [
+				{
+					name: blankField,
+					value: blankField,
+					inline: true
+				},
+				{
+					name: 'Total members:',
+					value: `${message.guild.memberCount}`,
+				}
+			],
 			timestamp: new Date()
 		};
 		message.channel.send({embed: embed});
