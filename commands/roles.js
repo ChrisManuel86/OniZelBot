5a5f1bca4841b4a5f061f8752f2d428ec8d25cf6
ChrisManuel86 <chris.manuel86@gmail,com>
2019-07-16 10:06:07 -0700

    pushing changes to current

--- commands/roles.js
+++ commands/roles.js
@@ -9,52 +9,52 @@ const Discord = require('discord.js');
 let embedError = new Discord.RichEmbed()
     .setColor(parseInt(embedColorValue));
 
-module.exports = {
+/** module.exports = {
     name: "roles",
     cooldown: 5,
     description: "Choose from the available roles on the server. See #available-roles for additional info on roles.",
     execute(message, args) {
         let { roles } = message.guild;
         // console.log(roles.map(role => role.name));
+    },
+    execute(message, args) {
+        // Create CONST commands object from client command
+        const { roles } = message.guild;
+        let perms = message.member.permissions;
+    
+        if (!args.length) {
+            // Create embedHelp object
+            let embedRoles = new Discord.RichEmbed()
+                .setColor(embedColorValue)
+                .setTitle('Help Command')
+                .setDescription('Here\'s a list of available roles.')
+                .addField('Available roles:', roles.map(role => role.name).join(', '))
+                .addField('Command usage:', `You can send \`${prefix}help [command name]\` to get info on a specific command!`)
+                .addField('Getting a role:', `In order to assign a role to yourself, send "${prefix} give [role name]", and the role will be assigned to you.`)
+                .addField('Removing a role:', `To remove a role`);
+    
+            return message.channel.send(embedRoles);
+        } else {
+            if (!role) {return message.reply('that role does not exist! Please chose a role from the available ones listed to you.');}
+            
+            // Create embedCommand object
+            let embedRole = new Discord.RichEmbed()
+                .setColor(embedColorValue)
+                .setTitle('**Command Name:**')
+                .setDescription(`${role.name}`)
+                .addBlankField();
+    
+            if (command.aliases) embedRole.addField('**Aliases:**', `${command.aliases.join(', ')}`);
+            if (command.description) embedRole.addField('**Description:**', `${command.description}`);
+            if (command.usage) embedRole.addField('**Usage:**', `${prefix}${command.name} ${command.usage}`);
+    
+            embedRole.addField('**Cooldown:**', `${command.cooldown || 3} second(s)`);
+            
+            return message.channel.send(embedRole);
+        }
     }
 };
-
-execute(message, args) {
-    // Create CONST commands object from client command
-    const { roles } = message.guild;
-
-    if (!args.length) {
-        // Create embedHelp object
-        let embedHelp = new Discord.RichEmbed()
-            .setColor(embedColorValue)
-            .setTitle('Help Command')
-            .setDescription('Here\'s a list of available roles.')
-            .addField('Available commands:', commands.map(command => command.name).join(', '))
-            .addField('Command usage:', `You can send \`${prefix}help [command name]\` to get info on a specific command!`);
-
-        return message.channel.send(embedHelp);
-    } else {
-        const name = args[0].toLowerCase();
-        const command = commands.get(name) || commands.find(c => c.aliases && c.aliases.includes(name));
-
-        if (!command) {return message.reply('that\'s not a valid command!');}			
-        
-        // Create embedCommand object
-        let embedCommand = new Discord.RichEmbed()
-            .setColor(embedColorValue)
-            .setTitle('**Command Name:**')
-            .setDescription(`${command.name}`)
-            .addBlankField();
-
-        if (command.aliases) embedCommand.addField('**Aliases:**', `${command.aliases.join(', ')}`);
-        if (command.description) embedCommand.addField('**Description:**', `${command.description}`);
-        if (command.usage) embedCommand.addField('**Usage:**', `${prefix}${command.name} ${command.usage}`);
-
-        embedCommand.addField('**Cooldown:**', `${command.cooldown || 3} second(s)`);
-        
-        return message.channel.send(embedCommand);
-    }
-}
+*/
 
 /*
 	usage: '[roles name]',
